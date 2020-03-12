fb9d1535fdd58e64614d3120d5237c6266be8e66
Berserk132 <ahmedsayed22988@yahoo.com>
2020-03-12 21:11:43 +0200

    Added Dynamic Slyder

--- js/app.js
+++ js/app.js
@@ -1,35 +1,53 @@
-// immediate invoked function expression
 
-(function() {
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
+let images = [];
+let currImage = 0;
 
-  //select both left and right button. Add event listeners
-  const buttons = document.querySelectorAll('.btn')
-  const imageDIV = document.querySelector('.img-container')
-  let counter = 0
+for (let i = 0; i < 5; i++){
 
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-      if (button.classList.contains('btn-left')){
-        counter--
-        if(counter < 0){
-          counter = pictures.length -1
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-      if (button.classList.contains('btn-right')){
-        counter++
-        if(counter > pictures.length -1){
-          counter = 0
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-    })
-  })
-})();
+    images.push(`../img/contBcg-${i}.jpeg`);
+}
+
+
+/*document.querySelector(".img-container").
+    style.
+    backgroundImage = 
+    "url(../img/contBcg-1.jpeg)";*/
+
+
+document.querySelector(".btn-left").addEventListener('click',()=>{
+
+    if (currImage == 0){
+        
+        currImage = images.length - 1;
+        document.querySelector(".img-container").
+        style.
+        backgroundImage = 
+        `url(${images[currImage]})`;
+        return;
+    }
+    currImage--;
+    document.querySelector(".img-container").
+        style.
+        backgroundImage = 
+        `url(${images[currImage]})`;
+
+});
+
+document.querySelector(".btn-right").addEventListener('click',()=>{
+
+    if (currImage == images.length - 1){
+        
+        currImage = 0;
+        document.querySelector(".img-container").
+        style.
+        backgroundImage = 
+        `url(${images[currImage]})`;
+        return;
+    }
+    currImage++;
+    document.querySelector(".img-container").
+        style.
+        backgroundImage = 
+        `url(${images[currImage]})`;
+
+});
